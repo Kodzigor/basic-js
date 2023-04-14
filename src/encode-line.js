6fb650e2a2c29a60a078d90d94fac784fdd3f4c6
Igor Kodzhebash <igorkodzebash@gmail.com>
2023-04-14 12:46:50 +0300

    made encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  const abc = "qwertyuiopasdfghjklzxcvbnm";
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let res = "";
+  if (str === "") {
+    return res;
+  }
+  str = str.split("");
+  let counter = 0;
+
+  let temp;
+  for (let i = 0; i < str.length; i++) {
+    if (abc.indexOf(str[i])) {
+      counter++;
+    }
+    if (str[i + 1] !== str[i]) {
+      if (counter < 2) {
+        res += str[i];
+      } else {
+        res += counter + str[i];
+      }
+      temp = counter;
+      counter = 0;
+    }
+  }
+  return res;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
