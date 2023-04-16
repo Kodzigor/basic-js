97fe9ed83455fe24c08d8ad45cdf08b422a6080f
Igor Kodzhebash <igorkodzebash@gmail.com>
2023-04-16 19:08:04 +0300

    done extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,56 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // Convert str and addition to string if they are not already
+  str = String(str);
+  let addition =
+    options && options.addition !== undefined ? String(options.addition) : "";
+
+  // Extract options or use default values
+  let repeatTimes =
+    options && options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  let separator =
+    options && options.separator !== undefined ? options.separator : "+";
+  let additionRepeatTimes =
+    options && options.additionRepeatTimes !== undefined
+      ? options.additionRepeatTimes
+      : 1;
+  let additionSeparator =
+    options && options.additionSeparator !== undefined
+      ? options.additionSeparator
+      : "|";
+
+  // Repeat the addition string
+  let repeatedAddition = addition;
+  if (additionRepeatTimes > 1) {
+    repeatedAddition = new Array(additionRepeatTimes)
+      .fill(addition)
+      .join(additionSeparator);
+  }
+
+  // Repeat the main string
+  let repeatedStr = new Array(repeatTimes)
+    .fill(str + repeatedAddition)
+    .join(separator);
+
+  return repeatedStr;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
